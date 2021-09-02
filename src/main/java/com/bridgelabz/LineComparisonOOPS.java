7949ce8ba20b84d53b251cfaf5c83cd9cca2c3fb
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 22:26:32 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/LineComparisonOOPS.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisonOOPS {
+    public static void main(String[] arg) {
+        System.out.println("Welcome to Line Comparison OOPS Program");
+    }
+}
