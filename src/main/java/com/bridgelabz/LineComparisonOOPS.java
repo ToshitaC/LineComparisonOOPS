ff8318d0f8d22d551407d7c2d3f0db5b3302abc1
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 23:12:35 +0530

    Added:Program for calculating length of line

--- src/main/java/com/bridgelabz/LineComparisonOOPS.java
+++ src/main/java/com/bridgelabz/LineComparisonOOPS.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparisonOOPS {
-    public static void main(String[] arg) {
-        System.out.println("Welcome to Line Comparison OOPS Program");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparison OOPS program");
+        LengthOfLine();
+    }
+
+    public static void LengthOfLine() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of X1:");
+        int x = sc.nextInt();
+        System.out.print("Enter the value of X2:");
+        int x1 = sc.nextInt();
+        System.out.print("Enter the value of Y1:");
+        int y = sc.nextInt();
+        System.out.print("Enter the value of Y2:");
+        int y1 = sc.nextInt();
+
+        double length = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+        System.out.println("Length = " + length);
     }
 }
