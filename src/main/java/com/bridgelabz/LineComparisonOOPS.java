985220420e3d3edf919faf1993ed517d95c0a1ce
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-03 09:02:46 +0530

    Added:Program for checking equality of line

--- src/main/java/com/bridgelabz/LineComparisonOOPS.java
+++ src/main/java/com/bridgelabz/LineComparisonOOPS.java
@@ -3,23 +3,42 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparisonOOPS {
+    public static int x1,x2,x3,x4,y1,y2,y3,y4;
+    public static double distance1,distance2;
     public static void main(String[] args) {
-        System.out.println("Welcome to Line comparison OOPS program");
-        LengthOfLine();
+        System.out.println("Welcome to Line comparison OOPS program !!");
+        input();
+        distance();
     }
 
-    public static void LengthOfLine() {
+    public static void input() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of X1:");
-        int x = sc.nextInt();
-        System.out.print("Enter the value of X2:");
-        int x1 = sc.nextInt();
-        System.out.print("Enter the value of Y1:");
-        int y = sc.nextInt();
-        System.out.print("Enter the value of Y2:");
-        int y1 = sc.nextInt();
+        System.out.print("Enter the value of x1: ");
+        x1 = sc.nextInt();
+        System.out.print("Enter the value of x2: ");
+        x2 = sc.nextInt();
+        System.out.print("Enter the value of x3 = ");
+        x3 = sc.nextInt();
+        System.out.print("Enter the value of x4 = ");
+        x4 = sc.nextInt();
+        System.out.print("Enter the value of y1 = ");
+        y1 = sc.nextInt();
+        System.out.print("Enter the value of y2 = ");
+        y2 = sc.nextInt();
+        System.out.print("Enter the value of y3 = ");
+        y3 = sc.nextInt();
+        System.out.print("Enter the value of y4 = ");
+        y4 = sc.nextInt();
+    }
 
-        double length = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
-        System.out.println("Length = " + length);
+    public static void distance()
+    {
+        distance1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Distance = " +distance1);
+        distance2=Math.sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
+        System.out.println("Distance = " +distance2);
+
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Distance = " + distance);
     }
 }
